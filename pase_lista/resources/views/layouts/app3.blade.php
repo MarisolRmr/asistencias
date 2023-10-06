497a14bd85f485021bee7e2b302aca49ad21f5d4
Sonia Muñoz <2030114@upv.edu.mx>
2023-10-06 10:54:37 -0600

    logout

--- pase_lista/resources/views/layouts/app3.blade.php
+++ pase_lista/resources/views/layouts/app3.blade.php
@@ -68,7 +68,7 @@
                     </li>
                     <li class="mt-0.5 w-full">
                         <a class="py-2.7   dark:opacity-80 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap rounded-lg px-4 font-semibold text-slate-700 transition-colors"
-                             href=" # ">
+                            href=" # ">
                             <div
                                 class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
@@ -80,7 +80,7 @@
                             <span class="ml-1 duration-300 opacity-100 pointer-events-none ease">Dashboard</span>
                         </a>
                         <a class="py-2.7   dark:opacity-80 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap rounded-lg px-4 font-semibold text-slate-700 transition-colors"
-                             href=" {{route("estudiante.asistencias.ver")}} ">
+                            href=" {{route("estudiante.asistencias.ver")}} ">
                             <div
                                 class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
@@ -93,7 +93,7 @@
                         </a>
 
                         <a class="py-2.7   dark:opacity-80 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap rounded-lg px-4 font-semibold text-slate-700 transition-colors"
-                             href=" {{route("logout")}} ">
+                            href=" {{route("logout")}} ">
                             <div
                                 class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
