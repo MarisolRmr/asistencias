987a4d2412d9e7e57f8d1017dee8340d864b79fd
Nubia Cantú <2030210@upv.edu.mx>
2023-10-05 22:04:24 -0600

    prueba clases

--- pase_lista/app/Models/Clase.php
+++ pase_lista/app/Models/Clase.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Clase extends Model
 {
     use HasFactory;
+    protected $table = 'clase';
+    protected $fillable = [
+        'id',
+        'hora_inicio',
+        'hora_fin',
+        'salon',
+        'dia',
+    ];
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'id_usuario');
+    }
+
 }
