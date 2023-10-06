5a3b962fffc85b14ee8926cf5c05ca7b1f709bc6
Nubia Cantú <2030210@upv.edu.mx>
2023-10-05 19:48:21 -0600

    prueba hora

--- pase_lista/app/Http/Controllers/ArduinoCOntroller.php
+++ pase_lista/app/Http/Controllers/ArduinoCOntroller.php
@@ -15,15 +15,30 @@ class ArduinoCOntroller extends Controller
             if ($dato == "1") {
                 $rfid = $request->input('rfid');
                 // Realiza la búsqueda en la tabla de la base de datos
-                $resultado = Arduino::where('codigo_tarjeta', $rfid)->first();
+                $usuario = Arduino::where('codigo_tarjeta', $rfid)->first();
 
                 // Comprueba si se encontró el dato en la base de datos
-                if ($resultado) {
-                    // Si se encontró, responde con "1" a Arduino
-                    return  "1";
+                if ($usuario) {
+                    // Si se encontró, obtén el rol del usuario
+                    $rol = $usuario->rol;
+                    // Si el rol es igual a 2, significa que es un usuario con el rol 2
+                    if ($rol == 2) {
+                        // Obten la hora actual
+                        $horaActual = now()->format('H:i:s');
+                        // Busca las clases relacionadas con el usuario y la hora actual dentro del rango
+                        $clases = Clase::where('id_usuario', $usuario->id)
+                            ->whereTime('hora_inicio', '<=', $horaActual)
+                            ->whereTime('hora_fin', '>=', $horaActual)
+                            ->get();
+                        // Responde con las clases encontradas
+                        return response()->json(['clases' => $clases, 'hora' => $horaActual]);
+                    } else {
+                        // Si no es un usuario con rol 2, responde con "Rol no válido" a Arduino
+                        return "Rol no válido";
+                    }
                 } else {
                     // Si no se encontró, responde con "2" a Arduino
-                    return  "2";
+                    return "2";
                 }
             } elseif ($dato == "2") {
                 $username = $request->input('username');
