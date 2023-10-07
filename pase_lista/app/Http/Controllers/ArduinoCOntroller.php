64638796a4a94742ce9f5a20eaa646c77603dfc0
Nubia Cantú <2030210@upv.edu.mx>
2023-10-07 15:25:08 -0600

    s

--- pase_lista/app/Http/Controllers/ArduinoCOntroller.php
+++ pase_lista/app/Http/Controllers/ArduinoCOntroller.php
@@ -95,18 +95,74 @@ class ArduinoCOntroller extends Controller
                 $username = $request->input('username');
                 $password = $request->input('password');
 
-                $user = Arduino::where('username', $username)->first();
+                $usuario = Arduino::where('username', $username)->first();
 
-                if (!$user) {
+                if (!$usuario) {
                     return "2";
                 }else{
                     if (Hash::check($password, $user->password)) {
+                        $DiaSemana = $request->input('DiaSemana');
+                        // Si se encontró, responde con "1" a Arduino
+                        if($usuario->rol==2){
+                            $clase = Clase::where('user_id', $usuario->id)
+                            ->where('hora_inicio', '<=', $hora)
+                            ->where('hora_fin', '>=', $hora)
+                            ->where('salon', $salon)
+                            ->where('dia', $DiaSemana)
+                            ->first();
+                            if ($clase){
+                                // Convierte la hora de string a objeto Carbon
+                                if ($clase->estado == "desactivada"){
+                                    $clase->estado = 'activada';
+                                    $clase->save(); 
+                                    return "4";
+                                }else{
+                                    $clase->estado = 'desactivada';
+                                    $clase->save();
+                                    return "5";
+                                }
+                            }else{
+                                
+                                return "6";
+                            }
+                            
+                        }else{
+                            $clase = Clase::where('salon', $salon)
+                            ->where('estado', 'activada')
+                            ->first();
+                            if ($clase){
+                                $asistenciaExistente = Asistencia::where('clase_id', $clase->id)
+                                ->where('user_id', $usuario->id)
+                                ->where('asistencia', 1)
+                                ->first();
+                                if (!$asistenciaExistente) {
+                                    $asistencia = new Asistencia([
+                                        'clase_id' => $clase->id,
+                                        'user_id' => $usuario->id,
+                                        'asistencia' => 1,
+                                        'fecha' => now(),
+                                    ]);
+                                
+                                    try {
+                                        $asistencia->save();
+                                        return "1";
+                                    } catch (\Exception $e) {
+                                        return "3";
+                                    }
+                                }else{
+                                    return "7";
+                                }
+                                
+                            }else{
+                                
+                                return "8";
+                            } 
+                        }
                         return "1";
                     }else{
                         return "2";
                     }
                 }
-
 
             }  elseif ($dato == "3") {
                 $clase = Clase::where('salon', $salon)
